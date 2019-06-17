d9e5dcda3569cb0212f9cf4fa2b12683295c21bc
Sneh Sameer <snehsameer0827@gmail.com>
2019-06-17 14:25:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,7 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-<title>Find Your Perfect Partner: Marital| Home  
-
+<title>Find Your Perfect Partner - Make My Love | Home :: 
 </title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -68,7 +67,7 @@ $(document).ready(function(){
   </div>
   <div class="container" style="position:absolute; width:600px; height:500px; right:0; top:0; margin-top: 80px;">
 		
-		<img style="height:500px; width:500px; overflow-x:hidden;" src="/images/undraw.svg" >
+		<img style="height:500px; width:500px; overflow-x:hidden; border-radius: 50%;" src="undraw.svg" >
 				
   </div>	
 </div>
@@ -76,12 +75,16 @@ $(document).ready(function(){
 <div class="grid_1">
       <div class="container">
       	<h1>Featured Profiles</h1>
+
+
        	<div class="heart-divider">
 			<span class="grey-line"></span>
 			<i class="fa fa-heart pink-heart"></i>
 			<i class="fa fa-heart grey-heart"></i>
 			<span class="grey-line"></span>
         </div>
+
+
         <ul id="flexiselDemo3">
         <?php
         	$sql="SELECT * FROM customer";
@@ -90,27 +93,27 @@ $(document).ready(function(){
         		while($row=mysqli_fetch_assoc($result)){
         			$name=$row['firstname'] . " " . $row['lastname'];
         			$profileid=$row['cust_id'];
-        			$age=$row['age'];
+        		
         			$place=$row['state'] . "," . $row['district'];
         			$job=$row['occupation'];
 
         				//getting profilepic
         				$pic1='';
-						$sql2="SELECT * FROM photos WHERE cust_id = $profileid";
-						$result2 = mysqlexec($sql2);
-						if($result2){
+						$sql2 = "SELECT * FROM images WHERE id='$profileid'" ;
+                        $result2 = mysqlexec($sql2);
+                        if($result2){
 							$row2=mysqli_fetch_array($result2);
-							$pic1=$row2['pic1'];
+							$pic1=$row2['image'];
 						}
 						//got profilepic
 						//
 					//Printing the html
 					echo "<li><div class=\"col_1\"><a href=\"view_profile.php?id={$profileid}\">";
-					echo "<img src=\"profile/{$profileid}\/{$pic1}\" alt=\"\" class=\"hover-animation image-zoom-in img-responsive\"/>";
+					echo "<img src=\"images/{$pic1}\" style=\"border-radius:50%;\" alt=\"\" class=\"hover-animation image-zoom-in img-responsive\"/>";
 					echo "<div class=\"layer m_1 hidden-link hover-animation delay1 fade-in\">";
-					echo "<div class=\"center-middle\">About {$name}</div>";
+					//echo "<div class=\"center-middle\">{$name}</div>";
 					echo "</div>";
-					echo "<h3><span class=\"m_3\">Profile ID : {$profileid}</span><br>{$age}, {$place}<br>{$job}</h3></a></div>";
+					echo "<h3><span class=\"m_3\"> Profile ID : {$profileid}</span><br> {$name} <br>  {$place}<br>{$job}</h3></a></div>";
 					echo "</li>";
 				
         		}
@@ -152,11 +155,7 @@ $(document).ready(function(){
 	
     <div class="footer">
     	<div class="container">
-    		<div class="col-md-4 col_2"></div>
-
-    			<h4><b>About Us</b></h4>
-    			<p>"Welcome to Marital.com Every life has its own charm in each stage and a married life is no exception. Marriage is an institution, which initiates the authentic values of life. In India, a marriage is usually arranged by the family or Relatives of the bride or the groom within a known circle. But today~s nomad life has narrowed this choice and hence matrimonial bureaus and marriage websites lead a vital role. Marital.com is celebrated as the most Trusted Matrimony Brand combining tradition and technology."</p>
-
+    		<div class="col-md-2 col_2">
     		</div>
     		<div class="col-md-2 col_2">
     			
@@ -164,8 +163,8 @@ $(document).ready(function(){
     		<div class="col-md-2 col_2">
     			
     		</div>
-    		<div class="col-md-2 col_2">
-    			<h4>Social</h4>
+    		<div  class="col-md-2 col_2">
+    			<h4 >Social</h4>
     			<ul class="footer_social">
 				  <li><a href="#"><i class="fa fa-facebook fa1"> </i></a></li>
 				  <li><a href="#"><i class="fa fa-twitter fa1"> </i></a></li>
@@ -175,14 +174,7 @@ $(document).ready(function(){
     		</div>
     		<div class="clearfix"> </div>
     		<div class="copy">
-			  <p>Designed by 
-			  <div class="heart-divider">
-				<span class="grey-line"></span>
-				<i class="fa fa-heart pink-heart"></i>
-				<i class="fa fa-heart grey-heart"></i>
-				<span class="grey-line"></span>
-			</div>
-			</p>
+			
 	        </div>
     	</div>
     </div>
